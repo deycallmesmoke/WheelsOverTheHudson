a241d3c8b9acf42b71be7d64784fbf4143dc1a09
jacobernst98 <jacobaernst@gmail.com>
2017-04-18 22:40:59 -0400

    iframe dom acception handeling

--- HTML/phpcart/index.php
+++ HTML/phpcart/index.php
@@ -38,6 +38,23 @@ if(isset($_GET['page'])){
 </head>
 
 <body>
+  <script>
+  window.addEventListener('message', function(event) {
+
+    // IMPORTANT: Check the origin of the data!
+    if (~event.origin.indexOf('http://yoursite.com')) {
+        // The data has been sent from your site
+
+        // The data sent with postMessage is stored in event.data
+        console.log(event.data);
+    } else {
+        // The data hasn't been sent from your site!
+        // Be careful! Do not use it.
+        return;
+    }
+});
+</script>
+
       <h1>Cart</h1>
 <?php
 
